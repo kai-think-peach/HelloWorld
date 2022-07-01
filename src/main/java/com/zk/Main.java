faca1be5cc32338bd8e6c536bbe74b755ad14172
kai-think-peach <08182851@cumt.edu.cn>
2022-07-01 18:01:09 +0800

    Java project github test.

---
+++ src/main/java/com/zk/Main.java
@@ -0,0 +1,13 @@
+package com.zk;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String[] strs={"aaa","bbb","ccc"};
+        for(String str:strs)
+        {
+            System.out.println(str);
+        }
+    }
+}
